07c0e9213334abb564198ff49d89fea9b48a660a
exuteboll <exuteboll4@gmail.com>
2025-05-22 11:11:11 +0300

    Add files via upload

---
+++ ConsoleApp7/NoteBook.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    struct NoteBook
+    {
+        public string Model { get; set; }
+        public string Proizv { get; set; }
+        public int Price { get; set; }
+
+        public NoteBook(string model, string proizv, int price )
+        {
+            Model = model;
+            Proizv = proizv;
+            Price = price;
+        }
+        public void Print()
+        { Console.WriteLine($"MOdel: {Model}\t Proizv: {Proizv}\t Price: {Price}"); }
+    }
+}
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NoteBook noteBook = new NoteBook();
+            noteBook.Price = 15523;
+            noteBook.Model = "Intel";
+            noteBook.Proizv = "amd";
+            noteBook.Print();
+            Console.ReadKey();
+        }
+    }
+}
