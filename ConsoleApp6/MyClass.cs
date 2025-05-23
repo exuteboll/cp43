2c7c93dbc81ede65164d3ba95c86f4b6e7484fcb
exuteboll <exuteboll4@gmail.com>
2025-05-23 11:40:02 +0300

    Update MyClass.cs

--- ConsoleApp6/MyClass.cs
+++ ConsoleApp6/MyClass.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp6
 {
+    /// <summary>
+    /// Структура с открытым полем change
+    /// <summary>
     class MyClass
     {
         public string change;
